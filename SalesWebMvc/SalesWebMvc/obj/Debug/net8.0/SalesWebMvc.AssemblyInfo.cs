e739a7d8d2465f5d5e42edcb37ca8fa124501fd3
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-02-03 13:34:53 -0300

    I managed to fix it

--- SalesWebMvc/SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001397d3e1e1d7a3c8fac6d69459d83d5dc78185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc5f6d967aeb77c45806b5b85e8a4f3a56fdc28")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
