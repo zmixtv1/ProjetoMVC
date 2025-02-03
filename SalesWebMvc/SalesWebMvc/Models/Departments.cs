6cc5f6d967aeb77c45806b5b85e8a4f3a56fdc28
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-02-03 13:04:44 -0300

    Everthing wrong

---
+++ SalesWebMvc/SalesWebMvc/Models/Departments.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesWebMvc.Models
+{
+    public class Departments
+    {
+        public int  Id { get; set; }
+        public string Name { get; set; }
+    }
+}
